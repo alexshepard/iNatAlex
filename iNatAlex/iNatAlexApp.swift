011036bb645f166c7460128e6269d1987f927ab7
Alex Shepard <alex@meatfreezer.com>
2025-03-04 09:38:35 -0800

    Initial Commit

---
+++ iNatAlex/iNatAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNatAlexApp.swift
+//  iNatAlex
+//
+//  Created by Alex Shepard on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iNatAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
