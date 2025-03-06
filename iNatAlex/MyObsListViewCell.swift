d156a2169fc6e1b1857393b8d2616792ace73776
Alex Shepard <alex@meatfreezer.com>
2025-03-06 14:44:09 -0800

    some ui tweaks to my obs cell

--- iNatAlex/MyObsListViewCell.swift
+++ iNatAlex/MyObsListViewCell.swift
@@ -11,19 +11,19 @@ struct MyObsListViewCell: View {
     @State var observation: INatObservation
 
     var body: some View {
-        HStack {
+        HStack(spacing: 10) {
             if let photoURL = observation.observationPhotos.first?.photo.smallUrl {
                 AsyncImage(url: photoURL) { image in
                     image
                         .resizable()
                         .scaledToFill()
                         .frame(width: 100, height: 100)
+                        .clipShape(RoundedRectangle(cornerRadius: 3.0))
                         .clipped()
                 } placeholder: {
                     ProgressView()
                         .frame(width: 100, height: 100)
                 }
-
             } else {
                 Text("No Photo")
                     .frame(width: 100, height: 100)
